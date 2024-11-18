da91e74d87eea4516f59952eccb9deacbc7213de
Kevin Rodrigues Borges <115191440+K3vinb5@users.noreply.github.com>
2024-11-18 22:41:58 +0000

    Added very cool changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Momô tá podi");
+        System.out.println("Momô não tá podi");
+        System.out.println(" O momo é cool");
     }
 }
