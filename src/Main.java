bf9a4ee8d6456323e8c61bc061f81232b1bd625f
Carol <carol@example.com>
2024-11-18 22:43:33 +0000

    linha conflituosa mujejeje

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Momô tá podi");
+        System.out.println("i23");
     }
 }
